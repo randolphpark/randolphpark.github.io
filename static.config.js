b2dc5cc54305a8a990adcc4a75aacf9154f90f86
Randolph Park <randolph.park@gmail.com>
2019-01-16 20:28:49 +1000

    Update config

---
+++ static.config.js
@@ -0,0 +1,13 @@
+// static.config.js
+
+export default {
+  paths: {
+    root: process.cwd(), // The root of your project. Don't change this unless you know what you're doing.
+    src: 'src', // The source directory. Must include an index.js entry file.
+    temp: 'tmp', // Temp output directory for build files not to be published.
+    dist: 'dist_source', // The production output directory.
+    devDist: 'tmp/dev-server', // The development scratch directory.
+    public: 'public', // The public directory (files copied to dist during build)
+    assets: 'dist', // The output directory for bundled JS and CSS
+  },
+}
