0305ee05bc11ca94ebb44f0e680e354ee75a3be9
Randolph Park <randolph.park@gmail.com>
2019-01-17 21:42:09 +1000

    Update config

--- static.config.js
+++ static.config.js
@@ -5,9 +5,10 @@ export default {
     root: process.cwd(), // The root of your project. Don't change this unless you know what you're doing.
     src: 'src', // The source directory. Must include an index.js entry file.
     temp: 'tmp', // Temp output directory for build files not to be published.
-    dist: 'dist_source', // The production output directory.
+    dist: 'dist', // The production output directory.
     devDist: 'tmp/dev-server', // The development scratch directory.
     public: 'public', // The public directory (files copied to dist during build)
     assets: 'dist', // The output directory for bundled JS and CSS
   },
+  siteRoot: 'https://www.randolphpark.me',
 }
